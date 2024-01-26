4783c75ed3465b8b2ab94193f5f6018b1fc583c0
Hlebushkek <volsoor@gmail.com>
2024-01-26 00:05:00 -0800

    add blob sas url retrieving function

---
+++ GetUserBlobSasUrl.cs
@@ -0,0 +1,63 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Identity;
+using Azure.Storage.Sas;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+using Azure.Storage;
+using System.Text;
+
+namespace PixelRenderer_AzureFunctions
+{
+    public class GetUserBlobSasUrl
+    {
+        private readonly ILogger _logger;
+
+        public GetUserBlobSasUrl(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<GetUserBlobSasUrl>();
+        }
+
+        [Function("GetUserBlobSasUrl")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function GetUserBlobSasUrl processed a request.");
+            
+            var user = await Helper.ValidateAndExtractUserAsync(req);
+            var containerName = $"userfiles-{user.Subject}";
+
+            string connectionString = Environment.GetEnvironmentVariable("BlobConnectionString", EnvironmentVariableTarget.Process);
+            var blobServiceClient = new BlobServiceClient(connectionString);
+
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+
+            var blobPath = req.Query["blobPath"];
+            var startDate = DateTimeOffset.UtcNow.AddMinutes(-1);
+            var expiryDate = DateTimeOffset.UtcNow.AddMinutes(10);
+
+            var sasBuilder = new BlobSasBuilder
+            {
+                BlobContainerName = containerName,
+                BlobName = blobPath,
+                Resource = "b", // "b" for blob, "c" for container
+                StartsOn = startDate,
+                ExpiresOn = expiryDate
+            };
+            sasBuilder.SetPermissions(BlobSasPermissions.Read | BlobSasPermissions.Write | BlobSasPermissions.Delete);
+
+            var sasToken = sasBuilder.ToSasQueryParameters(new StorageSharedKeyCredential(blobServiceClient.AccountName, "Unp8Muly8GPMmN24Oc61wbCwBCv+EpObRuhUf9mAUiPHCYnm9+ws12HVnTmlkTRo5WQPDYqNZ6MT+AStLFlKzQ==")).ToString();
+
+            var frontDoorUrl = Environment.GetEnvironmentVariable("STORAGE_URL");
+            var sasUrl = $"{frontDoorUrl}/{containerName}/{blobPath}?{sasToken}";
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString(sasUrl);
+            return response;
+        }
+    }
+}
---
+++ Helper.cs
@@ -0,0 +1,86 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Runtime.Intrinsics.Arm;
+
+
+namespace PixelRenderer_AzureFunctions
+{
+    internal static class Helper
+    {
+        private static readonly IConfigurationManager<OpenIdConnectConfiguration> _configurationManager;
+        private static readonly String jwksUri = "https://hlebushek.b2clogin.com/hlebushek.onmicrosoft.com/B2C_1_susi/discovery/v2.0/keys";
+        static Helper()
+        {
+            var documentRetriever = new HttpDocumentRetriever { RequireHttps = jwksUri.StartsWith("https://") };
+            _configurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(jwksUri, new OpenIdConnectConfigurationRetriever(), documentRetriever);
+        }
+
+        private static async Task<SecurityKey> GetSigningKeyAsync(string kid)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetStringAsync("https://hlebushek.b2clogin.com/hlebushek.onmicrosoft.com/B2C_1_susi/discovery/v2.0/keys");
+                var jsonWebKeySet = new JsonWebKeySet(response);
+                var signingKey = jsonWebKeySet.Keys.FirstOrDefault(k => k.KeyId == kid);
+                return signingKey;
+            }
+
+            /* var discoveryDocument = await _configurationManager.GetConfigurationAsync(CancellationToken.None);
+            if (discoveryDocument.AdditionalData.TryGetValue("keys", out var keys))
+            {
+                var jsonKeys = keys.ToString();
+                try {
+                    var jsonStr = discoveryDocument.AdditionalData.ToString();
+                    var jsonWebKeySet = new JsonWebKeySet(jsonStr);
+                    var signingKey = jsonWebKeySet.Keys.FirstOrDefault(k => k.KeyId == kid);
+                    return signingKey;
+                } catch(Exception ex) {
+                    Console.Out.WriteLine(ex.Message);
+                }
+            }
+
+            return nil*/
+        }
+
+        public static async Task<JwtSecurityToken> ValidateAndExtractUserAsync(Microsoft.Azure.Functions.Worker.Http.HttpRequestData req)
+        {
+            string authorizationHeader = req.Headers.GetValues("Authorization").FirstOrDefault();
+
+            if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
+            {
+                throw new Exception("No token provided in request headers");
+            }
+
+            string token = authorizationHeader.Substring("Bearer ".Length).Trim();
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = tokenHandler.ReadJwtToken(token) ?? throw new Exception("Invalid token");
+
+            var signingKey = await GetSigningKeyAsync(jwtToken.Header.Kid);
+
+            TokenValidationParameters validationParameters = new()
+            {
+                RequireExpirationTime = true,
+                RequireSignedTokens = true,
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateLifetime = true,
+                IssuerSigningKey = signingKey
+            };
+
+            try
+            {
+                tokenHandler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+                return (JwtSecurityToken)validatedToken;
+            }
+            catch (Exception)
+            {
+
+                throw new Exception("Token validation failed");
+            }
+        }
+    }
+}
